edd3ed84350afd1008cb3d19914ddd7fbf80e34d
Roberto Jr <113848806+spinolajunior@users.noreply.github.com>
2025-07-01 21:22:20 -0300

    Created FacrotyDao!

---
+++ src/model/dao/FactoryDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class FactoryDao {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+
+    public static DepartmentDao createDepartmentDao(){
+        return  new DepartmentDaoJDBC();
+    }
+}
